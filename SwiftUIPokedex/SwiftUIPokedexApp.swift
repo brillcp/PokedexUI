8a74b6bfe2d18c1d181514cf3a79e2cc575befdd
Viktor G <viktor@gidlof.se>
2021-11-12 16:12:48 +0100

    Initial Commit

---
+++ SwiftUIPokedex/SwiftUIPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPokedexApp.swift
+//  SwiftUIPokedex
+//
+//  Created by Viktor Gidlöf on 2021-11-12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
