3c43f9b3dec8b469763299bed60efa08128c91e7
VG <15960525+brillcp@users.noreply.github.com>
2025-07-18 17:20:22 +0200

    Refactors AudioPlayer for clarity and control
    Improves the `AudioPlayer` actor by adding documentation and enhancing control over audio playback.
    
    This update introduces clearer descriptions for the actor's properties and functions. It also ensures proper audio session configuration and provides methods for starting, stopping, and managing audio playback from URLs.

--- SwiftUIPokedex/Misc/AudioPlayer.swift
+++ SwiftUIPokedex/Misc/AudioPlayer.swift
@@ -1,16 +1,23 @@
 import AVFoundation
 
+/// An actor responsible for managing audio playback using AVFoundation.
 actor AudioPlayer {
     // MARK: Private properties
+    
+    /// The AVFoundation player instance used to control audio playback.
     private var player: AVPlayer?
 
     // MARK: - Public properties
+    
+    /// A Boolean value indicating whether the audio player is currently playing.
     var isPlaying: Bool {
         guard let player else { return false }
         return player.timeControlStatus == .playing
     }
 
     // MARK: init
+    
+    /// Initializes a new audio player and configures the audio session for playback.
     init() {
         let shared = AVAudioSession.sharedInstance()
         try? shared.setCategory(.playback, mode: .default)
@@ -20,6 +27,11 @@ actor AudioPlayer {
 
 // MARK: - Public functions
 extension AudioPlayer {
+    /// Begins playback of audio from the specified URL string.
+    ///
+    /// If playback is already in progress, it stops the current audio before starting the new one.
+    ///
+    /// - Parameter url: A string representing the URL of the audio to play.
     func play(from url: String) {
         guard let url = URL(string: url) else { return }
 
@@ -32,8 +44,10 @@ extension AudioPlayer {
         player?.play()
     }
 
+    /// Stops the current audio playback and resets the position to the beginning.
     func stop() {
         player?.pause()
         player?.seek(to: .zero)
     }
 }
+
