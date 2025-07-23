a93e5df9cb7790d2d64cdc7e2da909e58e788f72
VG <15960525+brillcp@users.noreply.github.com>
2025-07-23 18:28:32 +0200

    Removes unnecessary @ViewBuilder attribute
    Removes the `@ViewBuilder` attribute from the `sprite` computed property in `ItemDetailRowView`.
    
    The `@ViewBuilder` attribute is implicitly applied to computed properties that return a `View`, making its explicit declaration redundant.

--- PokedexUI/CustomUI/ItemDetailRowView.swift
+++ PokedexUI/CustomUI/ItemDetailRowView.swift
@@ -24,7 +24,6 @@ struct ItemDetailRowView: View {
 
 // MARK: - Private properties
 private extension ItemDetailRowView {
-    @ViewBuilder
     var sprite: some View {
         Group {
             if let image {
